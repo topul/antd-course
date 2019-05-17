dd850cf168d4742b99e38ec1e7cf0f652096992f
黄亚飞 <huangyafei837@pingan.com.cn>
2019-05-17 21:44:15 +0800

    edit

--- config/config.js
+++ config/config.js
@@ -1,10 +1,55 @@
 export default {
 	singular: true,
-	plugins: [["umi-plugin-react", {}]],
+	plugins: [
+		[
+			"umi-plugin-react",
+			{
+				antd: true,
+				dva: true
+			}
+		]
+	],
 	routes: [
 		{
 			path: "/",
-			component: "./HelloWorld"
+			component: "../layout",
+			routes: [
+				{
+					path: "/",
+					component: "HelloWorld"
+				},
+				{
+					path: "helloworld",
+					component: "./HelloWorld"
+				},
+				{
+					path: "/dashboard",
+					routes: [
+						{
+							path: "/dashboard/analysis",
+							component: "Dashboard/Analysis"
+						},
+						{
+							path: "/dashboard/monitor",
+							component: "Dashboard/Monitor"
+						},
+						{
+							path: "/dashboard/workplace",
+							component: "Dashboard/Workplace"
+						}
+					]
+				},
+				{
+					path: "puzzlecards",
+					component: "./puzzlecards"
+				}
+			]
 		}
-	]
+	],
+	// proxy: {
+	// 	'/random_joke': {
+	// 		target: 'https://official-joke-api.appspot.com',
+	// 		changeOrigin: true
+	// 	}
+	// }
 };
---
+++ src/util/request.js
@@ -0,0 +1,17 @@
+function checkStatus(response) {
+	if (response.status >= 200 && response.status < 300) {
+		return response;
+	}
+}
+
+/**
+ * Requests a URL, returning a promise
+ * @param {string} url The URL we want to request
+ * @param {object} [options] The options we want to pass to 'fetch'
+ * @return {object}	An object containing either 'data' or 'err'
+ */
+export default async function request(url, options) {
+	const response = await fetch(url, options);
+	checkStatus(response);
+	return await response.json();
+}
